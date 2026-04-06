b44a7be29df5484bd6096cd4ca8c9a71f35449a2
ikimyuseong <jbkys0714@icloud.com>
2026-04-06 17:53:12 +0900

    Update TestMyBranch.cs

--- Assets/Script/TestMyBranch.cs
+++ Assets/Script/TestMyBranch.cs
@@ -5,12 +5,12 @@ public class TestMyBranch : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("hello");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
